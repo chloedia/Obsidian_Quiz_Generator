f0b4c3ea3ad0f4941cf3caa5de6bb29a13045420
chloedia <chloe.daems@edu.devinci.fr>
2023-08-16 15:14:20 +0200

    Upgrade question quality

--- src/quiz_generator.ts
+++ src/quiz_generator.ts
@@ -1,155 +1,187 @@
-import {App,addIcon, Notice, Plugin, PluginSettingTab, Setting, request, MarkdownView, Editor, parseFrontMatterAliases, MetadataCache} from 'obsidian';
-import QuizGenPlugin from './main'
+import {
+	App,
+	Notice,
+	request,
+} from "obsidian";
+import QuizGenPlugin from "./main";
 import debug from "debug";
-import ReqFormatter from './req_formatter';
-import { TIMEOUT } from 'dns';
-var _ = require('underscore');
-const logger = debug('quizgenerator: QuizGenerator');
+import ReqFormatter from "./req_formatter";
+import * as _ from 'underscore';
 
 
-export default class QuizGenerator{
-    plugin: QuizGenPlugin;
-    app: App;
-    n_gen_question : number;
+const logger = debug("quizgenerator: QuizGenerator");
 
-    constructor(app: App, plugin: QuizGenPlugin) {
-        this.app = app;
+export default class QuizGenerator {
+	plugin: QuizGenPlugin;
+	app: App;
+	n_gen_question: number;
+
+	constructor(app: App, plugin: QuizGenPlugin) {
+		this.app = app;
 		this.plugin = plugin;
-        this.n_gen_question = 0;
+		this.n_gen_question = 0;
 	}
 
-    async generate(title: string):Promise<string[]> {
-        logger(`Generating a Quiz on ${title}`);
-        if (!this.plugin.processing) {
-            this.plugin.processing = true
-            // We get the text of the app
-            const currentFile = this.app.workspace.getActiveFile();
-            if (!currentFile) return [""];
-    
-            const content = await this.app.vault.read(currentFile);
-    
-            // We preprocess it (split the text in chunks of 2000 characters)
-            const chunks = this.preprocessText(content, 2000);
-    
-            // Get the responses for each chunk
-            let responses: string[] = [];
-            
-    
-            await Promise.all(chunks.map(async (chunk) =>{
+	async generate(title: string): Promise<string[]> {
+		logger(`Generating a Quiz on ${title}`);
+		if (!this.plugin.processing) {
+			this.plugin.processing = true;
+			// We get the text of the app
+			const currentFile = this.app.workspace.getActiveFile();
+			if (!currentFile) return [""];
 
-                let trans_chunk = chunk.replace(/\"/gm, '*')
-                trans_chunk = chunk.replace(/\'/gm, '_')
+			const content = await this.app.vault.read(currentFile);
 
-                this.plugin.settings.prompt = this.getPrompt(trans_chunk);
-                let reqformatter = new ReqFormatter(this.app, this.plugin);
-                const params = reqformatter.prepareReqParameters(this.plugin.settings, false);
-                const response = await this.getQuizFromAPI(params);
+			// We preprocess it (split the text in chunks of 2000 characters)
+			const chunks = this.preprocessText(content, 2000);
 
-                responses = [...responses, ...response]
-                this.n_gen_question += 5;
-                // Delay the execution of each iteration by 3 seconds
-                //await this.delay(3000);
-            }))
-    
-            // Combine the responses
-            //const combinedResponse = this.combineResponses(responses);
-    
-            // Return the combined response
-            return responses;
-        } else {
-            new Notice("There is already another generation process");
-            logger("generate error", "There is another generation process");
-            return Promise.reject(new Error("There is another generation process"));
-        }
-    }
-    async prune_question(text: string[]): Promise<string[]> {
-        console.log(`Currently Pruning to 10 questions ...`)
-        if (this.n_gen_question > 10){
-            return _.sample(text, 10)
-        }
-        return text
-    }
-    
-    preprocessText(text: string, chunkSize: number): string[] {
-        const chunks: string[] = [];
-        const paragraphs: string[] = text.split("\n"); // Split text into paragraphs
-    
-        let currentChunk = "";
-    
-        for (const paragraph of paragraphs) {
-            if (currentChunk.length + paragraph.length > chunkSize) {
-                chunks.push(currentChunk.trim());
-                currentChunk = "";
-            }
-    
-            currentChunk += paragraph + " ";
-        }
-    
-        if (currentChunk !== "") {
-            chunks.push(currentChunk.trim());
-        }
-    
-        return chunks;
-    }
-    
-    combineResponses(responses: string[]): string {
-        return responses.join('\n');
-    }
+			// Get the responses for each chunk
+			let responses: string[] = [];
 
-    getPrompt(content : string){
-        return '[INPUT][N_QUESTIONS = 5]' + content
-    }
+			await Promise.all(
+				chunks.map(async (chunk) => {
+					let trans_chunk = chunk.replace(/"/gm, "*");
+					trans_chunk = chunk.replace(/'/gm, "_");
 
-    async getQuizFromAPI(params: any, n_try: number = 0): Promise<string[]> {
-        // Send request to OpenAI's API to generate the quiz
-        let response = await request(params);
-        const response_json = JSON.parse(response);
-        response = response_json.choices[0].message.content
+					this.plugin.settings.prompt = this.getPrompt(trans_chunk);
+					const reqformatter = new ReqFormatter(
+						this.app,
+						this.plugin
+					);
+					const params = reqformatter.prepareReqParameters(
+						this.plugin.settings,
+						false
+					);
+					const response = await this.getQuizFromAPI(params);
 
-        try {
-          let assistantResponse = await this.outputFormatting(response)
+					responses = [...responses, ...response];
+					this.n_gen_question += 5;
+					// Delay the execution of each iteration by 3 seconds
+					//await this.delay(3000);
+				})
+			);
 
-          // Return the assistant response
-          return assistantResponse;
+			// Combine the responses
+			//const combinedResponse = this.combineResponses(responses);
 
-        } catch (error) {
-          n_try += 1
-          if (n_try > 4){
-            this.plugin.processing = false
-            new Notice("We are having trouble creating the quiz, please try again ...");
-            throw error}
-            
-          console.log(`N TRY : ${n_try} ! The json was not correct ... Reformulating ... ${error}`)
-          const new_prompt = `This is not a correct json ! Return a corrected version format (to help you the error is ${error}) : ${response}`
-    
-          params.prompt = new_prompt
-          const assistantResponse = this.getQuizFromAPI(params, n_try)
-          return assistantResponse
-        }
-      }
-    
-      async outputFormatting(input: string){
-        //Function to format the output
-        let result:string[] = [];
-        let transformedString = input.replace('[OUTPUT]',"")
-        transformedString = transformedString.replace(/,(?=[}\]])/gm,"")
+			// Return the combined response
+			return responses;
+		} else {
+			new Notice("There is already another generation process");
+			logger("generate error", "There is another generation process");
+			return Promise.reject(
+				new Error("There is another generation process")
+			);
+		}
+	}
+	async prune_question(text: string[]): Promise<string[]> {
+		console.log(`Currently Pruning to 10 questions ...`);
+		if (this.n_gen_question > 10) {
+			return _.sample(text, 10);
+		}
+		return text;
+	}
 
+	preprocessText(text: string, chunkSize: number): string[] {
+		const chunks: string[] = [];
+		const paragraphs: string[] = text.split("\n"); // Split text into paragraphs
+		if (paragraphs[paragraphs.length - 1].length == 0){
+			paragraphs.pop()
+		}
 
-        let jsonResult = JSON.parse(transformedString);
-        
+		let currentChunk = "";
 
-        for (let entry of jsonResult.Questions) {
-            if (entry.answer != ""){
-            let new_set = `${JSON.stringify(entry.question).replace(/\\\\/gm,"\\")}\n?\n${JSON.stringify(entry.answer).replace(/\\\\/gm,"\\")} (Exact Quote : \"${entry.quote != ""? entry.quote : "NA"})\"\n\n`;
-            result.push(new_set.replace(/\"/gm, ''));
-            }
-          }
+		for (const paragraph of paragraphs) {
+			if (currentChunk.length + paragraph.length > chunkSize) {
+				chunks.push(currentChunk.trim());
+				currentChunk = "";
+			}
 
-        return result;
+			currentChunk += paragraph + " ";
+		}
 
-    }
-    delay(ms: number): Promise<void> {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-    }
+		if (currentChunk !== "") {
+			chunks.push(currentChunk.trim());
+		}
+		console.log(chunks)
+		return chunks;
+	}
+
+	combineResponses(responses: string[]): string {
+		return responses.join("\n");
+	}
+
+	getPrompt(content: string) {
+		return "[INPUT]" + content;
+	}
+
+	async getQuizFromAPI(params: any, n_try = 0): Promise<string[]> {
+		// Send request to OpenAI's API to generate the quiz
+		let response = await request(params);
+		const response_json = JSON.parse(response);
+		response = response_json.choices[0].message.content;
+		response = response.replace(/(?<!\\)\\(?=[a-zA-Z])/gm, "\\\\")
+		let assistantResponse = [""]
+
+		try {
+			assistantResponse = await this.outputFormatting(response);
+
+			// Return the assistant response
+			return assistantResponse;
+		} catch (error) {
+			n_try += 1;
+			if (n_try > 1) {
+				this.plugin.processing = false;
+				new Notice(
+					"We are having trouble creating the quiz, some part of the text might not have flashcards please try again by selecting a specific part of the text."
+				);
+				assistantResponse = [""]
+				//throw error;
+			}else{
+
+			console.log(
+				`N TRY : ${n_try} ! The json was not correct ... Reformulating ... ${error}`
+			);
+			const new_prompt = `This is not a correct json ! Return a corrected version format (to help you the error is ${error}) : ${response}`;
+
+			params.prompt = new_prompt;
+			assistantResponse = await this.getQuizFromAPI(params, n_try);
+			}
+			return assistantResponse;
+		}
+	}
+
+	async outputFormatting(input: string) {
+		//Function to format the output
+		const result: string[] = [];
+		let transformedString = input.replace("[OUTPUT]", "");
+		transformedString = transformedString.replace(/,(?=[}\]])/gm, "");
+
+		console.log(transformedString)
+		if (transformedString == "{}"){
+			return [""]
+		}
+
+		const jsonResult = JSON.parse(transformedString);
+
+		for (const entry of jsonResult.Questions) {
+			if (entry.answer != "" && entry.answer != "null" ) {
+				const new_set = `${JSON.stringify(entry.question).replace(
+					/\\\\/gm,
+					"\\"
+				)}\n?\n${JSON.stringify(entry.answer).replace(
+					/\\\\/gm,
+					"\\"
+				)} (Exact Quote : "${
+					(entry.key_info != "") && (!entry.key_info.includes("pyramids")) ? entry.key_info : "NA"
+				})"\n\n`;
+				result.push(new_set.replace(/"/gm, ""));
+			}
+		}
+
+		return result;
+	}
+	delay(ms: number): Promise<void> {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
 }
-
--- src/types.ts
+++ src/types.ts
@@ -1,70 +1,71 @@
-    type QuizGeneratorSettings= {
-		api_key: string;
-		engine: string;
-		max_tokens: number;
-		temperature: number;
-		frequency_penalty: number;
-		system_prompt: string;
-		prompt: string;
-		n_questions: number;
-		prune: boolean;
-		promptsPath: string;
-		showStatusBar: boolean;
-		displayErrorInEditor: boolean;
-		outputToBlockQuote: boolean;
-  }
-  
-  type QuizGeneratorConfiguration = {
+type QuizGeneratorSettings = {
+	api_key: string;
+	engine: string;
+	max_tokens: number;
+	temperature: number;
+	frequency_penalty: number;
+	system_prompt: string;
+	prompt: string;
+	n_questions: number;
+	prune: boolean;
+	promptsPath: string;
+	showStatusBar: boolean;
+	displayErrorInEditor: boolean;
+	outputToBlockQuote: boolean;
+};
+
+type QuizGeneratorConfiguration = {
 	packages: PackageTemplate[];
 	installedPackages: InstalledPackage[];
-  }
-  
-  type InstalledPackage = {
-	packageId:string;
-	version:string
-	prompts:PromptTemplate[];
-	installedPrompts: installedPrompts []
-  }
-  
-  type installedPrompts ={
+};
+
+type InstalledPackage = {
+	packageId: string;
+	version: string;
+	prompts: PromptTemplate[];
+	installedPrompts: installedPrompts[];
+};
+
+type installedPrompts = {
 	promptId: string;
 	version: string;
-  }
-  
-  type PackageTemplate = {
+};
+
+type PackageTemplate = {
 	packageId: string;
 	name: string;
-	version:  string;
-	  minTextGeneratorVersion:  string;
-	  description:  string;
-	tags :  string;
-	author:  string;
-	  authorUrl: string;
+	version: string;
+	minTextGeneratorVersion: string;
+	description: string;
+	tags: string;
+	author: string;
+	authorUrl: string;
 	repo: string;
 	published_at: Date;
-	downloads:number;
-  }
-  
-  type PromptTemplate =  {
-	  promptId: string;
-	  name: string;
-	  path: string;
-	  description: string;
-	  required_values: string;
-	  author: string;
-	  tags: string;
-	  version: string;
-	}
-  
-  type FileViewMode = 'source' | 'preview' | 'default';
-  enum NewTabDirection {
-	vertical = "vertical", horizontal = "horizontal"
-  }
-  
-  type Model = {
+	downloads: number;
+};
+
+type PromptTemplate = {
+	promptId: string;
+	name: string;
+	path: string;
+	description: string;
+	required_values: string;
+	author: string;
+	tags: string;
+	version: string;
+};
+
+type FileViewMode = "source" | "preview" | "default";
+enum NewTabDirection {
+	vertical = "vertical",
+	horizontal = "horizontal",
+}
+
+type Model = {
 	id: string;
-  }
-  export type {
+};
+export type {
 	FileViewMode,
 	NewTabDirection,
 	QuizGeneratorSettings,
@@ -72,5 +73,5 @@
 	PackageTemplate,
 	Model,
 	InstalledPackage,
-	QuizGeneratorConfiguration
-  }
+	QuizGeneratorConfiguration,
+};
